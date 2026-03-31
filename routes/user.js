bed9cc26ddb15866a37f022c26654bd4681f54a1
arthur070809 <abaldotedesco@gmail.com>
2026-03-31 17:19:43 -0300

    sd

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,73 @@
 const express = require('express');
-const router = express.Router();
+const routes = express.Router();
 const db = require('../db');
+const { routes } = require('../app');
 
-router.get('/', (req, res) => {
+
+//Get em usuários
+routes.get('/', (req, res) => {
     db.query('SELECT * FROM users', (err, results) => {
         if (err) {
-            res.status(500).json({ error: 'Erro ao buscar usuários' });
+            res.status(500).json({ error: 'Erro ao buscar usuários' })
         } else {
-            res.json(results);
+            res.json(results)
         }
     });
 });
 
-module.exports = router;
+//Criar um novo usuario usuario
+routes.post('/create', (req, res) => {
+    const { nome, email, senha } = req.body;
+    db.query('SELECT * FROM usuarios (nome, email, senha) VALUES (?, ?, ?)',
+        [nome, email, senha], (err, results) => {
+        if (err) {
+            res.status(500).json({ error: 'Erro ao buscar usuários' });
+        } else {
+            res.status(201).json({ id: results.insertId, nome, email});
+        }
+    });
+});
+
+//Editar um usuario
+routes.put('/edit/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, email, senha } = req.body;
+    db.query('UPDATE usuarios SET nome = ?, email = ?, senha = ? WHERE id = ?',
+        [nome, email, senha, id], (err, results) => {
+        if (err) {
+            res.status(500).json({ error: 'Erro ao buscar usuários' });
+        } else {
+            res.status(201).json({ id, nome, email});
+        }
+    });
+});
+
+//deletar um usuario
+routes.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    db.query('DELETE FROM usuarios WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            res.status(500).json({ error: 'Erro ao buscar usuários' });
+        } else {
+            res.status(201).json({ message: 'Usuário deletado com sucesso' });
+        }
+    });
+});
+
+//Buscar um usuario por id
+routes.get('/:id', (req, res) => {
+    const { id } = req.params;
+    db.query('SELECT * FROM usuarios WHERE id = ?', [id], (err, results) => {
+        if (err) {
+            res.status(500).json({ error: 'Erro ao buscar usuários' });
+        } else {
+            if (results.length === 0) {
+                res.status(400).json({ error: 'Usuário não encontrado' });
+            } else {
+                res.status(201).json(results[0]);
+            }
+        }
+    });
+});
+
+module.exports = routes;
